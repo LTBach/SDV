0319e2fa051678350cdb7d83f98ad3978b6fc407
LTBach <21521845@gm.uit.edu.vn>
2022-10-22 21:03:29 +0700

    Update_Class_2_Code

--- Part_1/Class_2/bai_3/src/main.rs
+++ Part_1/Class_2/bai_3/src/main.rs
@@ -6,6 +6,7 @@ pub fn iter_num(num: i32) -> bool {
 
     let num_str = num.to_string();
     let chars = num_str.chars(); // <-- move occurs because `chars` has type `Chars<'_>`, which does not implement the `Copy` trait
+    let chars_2 = num_str.chars();
     let len =  chars.count();     // <-- `chars` moved due to this method call
 
     println!("Len = {:?}", len);
